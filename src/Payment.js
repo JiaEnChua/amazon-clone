1011ba3b54b21257347a92b7fbdef371cdd5cc17
jiaenchua <jiaen_je@hotmail.com>
2020-09-22 19:16:02 -0500

    firebase hosting and payment page

--- src/Payment.js
+++ src/Payment.js
@@ -2,12 +2,17 @@ import React from "react";
 import "./Payment.css";
 import { useStateValue } from "./StateProvider";
 import CheckoutProduct from "./CheckoutProduct";
+import { Link } from "react-router-dom";
 
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
   return (
     <div className="payment">
       <div className="payment__container">
+        <h1>
+          Checkout (<Link to="/checkout">{basket?.length} items</Link>)
+        </h1>
+
         <div className="payment__section">
           <div className="payment__title">
             <h3>Delivery Address</h3>
@@ -36,7 +41,12 @@ function Payment() {
           </div>
         </div>
 
-        <div className="payment__section"></div>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details">{/* Stripe magic */}</div>
+        </div>
       </div>
     </div>
   );
