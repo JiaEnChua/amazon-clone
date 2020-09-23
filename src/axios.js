9896842e9922b7356d8d6fdd1a3a1eca6aa7696c
jiaenchua <jiaen_je@hotmail.com>
2020-09-23 14:28:36 -0500

    deployed firebase cloud fn

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/clone-b869b/us-central1/api", //The API (cloud fn) URL
+  baseURL: "https://us-central1-clone-b869b.cloudfunctions.net/api",
+  // "http://localhost:5001/clone-b869b/us-central1/api", //The API (cloud fn) URL
 });
 
 export default instance;
